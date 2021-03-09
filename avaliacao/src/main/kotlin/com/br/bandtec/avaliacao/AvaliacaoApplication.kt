5cba638533d116863c5f560974cb5fad8906605f
Rubens Silva <re92253z@BR2L11891L.nimbus.local>
2021-03-08 21:03:18 -0300

    spring

---
+++ avaliacao/src/main/kotlin/com/br/bandtec/avaliacao/AvaliacaoApplication.kt
@@ -0,0 +1,11 @@
+package com.br.bandtec.avaliacao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvaliacaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvaliacaoApplication>(*args)
+}
---
+++ avaliacao/src/test/kotlin/com/br/bandtec/avaliacao/AvaliacaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.bandtec.avaliacao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvaliacaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
